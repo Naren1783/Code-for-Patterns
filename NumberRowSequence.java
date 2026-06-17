9fa76615e3a4015f919e12535b033fb692ff115b
Naren B <bnaren18@gmail.com>
2026-06-17 22:51:05 +0530

    Define NumberRowSequence class

---
+++ NumberRowSequence.java
@@ -0,0 +1,8 @@
+public class NumberRowSequence{
+    public static void main(String[] args){
+        int n=7;
+        for(int j=1;j<=n;j++){
+            System.out.print(j+" ");
+        }
+    }
+}
