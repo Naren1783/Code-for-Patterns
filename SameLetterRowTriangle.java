bb8afb3c9add68d9e7daa74d17d933fa5812a345
Naren B <bnaren18@gmail.com>
2026-06-18 15:46:50 +0530

    SameLetterRowTriangle

---
+++ SameLetterRowTriangle.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class SameLetterRowTriangle.java{
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the size : ");
+        int n= scanner.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print((char)(i+64)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+//OUTPUT:
+//Enter the size : 
+//5
+//  A 
+//  B B 
+//  C C C 
+//  D D D D 
+//  E E E E E 
