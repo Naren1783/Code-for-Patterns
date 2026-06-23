24c2a87e8288257957ed789ac88739f0d57b70ab
Naren B <bnaren18@gmail.com>
2026-06-23 22:12:05 +0530

    ConsecutiveNumberTriangle

---
+++ ConsecutiveNumberTriangle.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class ConsecutiveNumberTriangle{
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        int n= scanner.nextInt();
+        int a=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(a+" ");
+                a++;
+            }
+            System.out.println();
+        }
+    }
+}
